e97710a8dad5dc6d2d1ea26d2fec94b91d05f8d8
ETXSVC <ddavis@etxsvc.com>
2025-11-18 22:42:12 -0600

    feat: Initialize Android application with WebView to display BudgetFlow web content.

---
+++ android/app/src/main/java/com/goodle/budgetflow/MainActivity.java
@@ -0,0 +1,39 @@
+package com.goodle.budgetflow;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        
+        // Ensure links open within the WebView
+        webView.setWebViewClient(new WebViewClient());
+
+        // Load the local index.html file
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
